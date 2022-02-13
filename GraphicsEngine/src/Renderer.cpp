08e0de88760208db1f7610ae1422a28eefe39b35
Simas Mikelionis <simasmik@gmail.com>
2022-02-13 15:57:26 +0200

    Added depth value testing

--- GraphicsEngine/src/Renderer.cpp
+++ GraphicsEngine/src/Renderer.cpp
@@ -19,7 +19,7 @@ bool GLLogCall(const char* function, const char* file, int line)
 
 void Renderer::Clear() const
 {
-    GLCall(glClear(GL_COLOR_BUFFER_BIT));
+    GLCall(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
 }
 
 void Renderer::Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const
@@ -30,5 +30,9 @@ void Renderer::Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& 
     ib.Bind();
 
     GLCall(glEnable(GL_CULL_FACE));
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+
     GLCall(glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, nullptr));
 }
