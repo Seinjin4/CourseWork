e81694b290e88798dee86235f21a60d3208ba329
Simas Mikelionis <simasmik@gmail.com>
2022-06-08 14:37:36 +0300

    Improved cyclical cube scene

--- GraphicsEngine/src/scenes/CyclidicCubeScene.cpp
+++ GraphicsEngine/src/scenes/CyclidicCubeScene.cpp
@@ -2,52 +2,10 @@
 #include <vendor/imgui/imgui.h>
 #include <vendor/MathCalculations.h>
 
-scenes::CyclidicCubeScene::CyclidicCubeScene() :
-    plain(30),
-    cyclideCubeSideShader("res/shaders/CyclideCubeSide.shader"),
-    v1(-1.0f, 0.0f, 0.0f),
-    v2(0.0f, 0.0f, -1.0f),
-    p3Slider(0.126f),
-    p5Slider(0.874f),
-    p6Slider(0.374f)
+void scenes::CyclidicCubeScene::RenderCube(const Renderer& renderer)
 {
-}
-
-void scenes::CyclidicCubeScene::RenderScene(const Renderer& renderer, glm::mat4 newView)
-{
-	glm::mat4& view = camera.GetView();
-	view = newView;
-
-    glm::vec3 viewPos = newView[3];
-    glm::vec3 lightPos = glm::vec3(0.0f, 10.0f, 16.0f);
-    glm::vec3 lightColor = glm::vec3(1.0f, 1.0f, 1.0f);
-
-    RenderGizmo(renderer, newView);
-
-
-    mathCalc::CircleData sideA = mathCalc::GetCircleDataFrom3Points(p0, p2, p4);
-    mathCalc::CircleData sideB = mathCalc::GetCircleDataFrom3Points(p0, p1, p2);
-    mathCalc::CircleData sideC = mathCalc::GetCircleDataFrom3Points(p0, p4, p1);
-
-    p6 = mathCalc::CalculatePointInCircle(sideA, p6Slider * glm::pi<float>() * 2);
-    p3 = mathCalc::CalculatePointInCircle(sideB, p3Slider * glm::pi<float>() * 2);
-    p5 = mathCalc::CalculatePointInCircle(sideC, p5Slider * glm::pi<float>() * 2);
-
-    mathCalc::CircleData sideF = mathCalc::GetCircleDataFrom3Points(p1, p3, p5);
-    mathCalc::CircleData sideE = mathCalc::GetCircleDataFrom3Points(p4, p5, p6);
-    mathCalc::CircleData sideD = mathCalc::GetCircleDataFrom3Points(p2, p3, p6);
-
-    mathCalc::Plane planeF = mathCalc::GetPlaneFromCircleData(sideF);
-    mathCalc::Plane planeE = mathCalc::GetPlaneFromCircleData(sideE);
-    mathCalc::Plane planeD = mathCalc::GetPlaneFromCircleData(sideD);
-
-    p7 = mathCalc::IntersectionPointFrom3Planes(
-        planeF,
-        planeE,
-        planeD
-    );
-
-    renderer.ToggleWireFrame(wireframeToggle);
+    glm::vec3 staticSideColor = glm::vec3(0.0f, 0.42f, 0.46f);
+    glm::vec3 dinamicSideColor = glm::vec3(0.51f, 0.77f, 0.74f);
 
     //Side A
     {
@@ -64,7 +22,7 @@ void scenes::CyclidicCubeScene::RenderScene(const Renderer& renderer, glm::mat4 
         cyclideCubeSideShader.SetUnifromVec3f("lightPos", lightPos);
         cyclideCubeSideShader.SetUnifromVec3f("viewPos", viewPos);
         cyclideCubeSideShader.SetUnifromVec3f("lightColor", lightColor);
-        cyclideCubeSideShader.SetUnifromVec3f("objectColor", glm::vec3(1.0f, 0.3f, 1.0f));
+        cyclideCubeSideShader.SetUnifromVec3f("objectColor", staticSideColor);
         renderer.DrawTriangles(plain.GetVertexArray(), plain.GetIndexBuffer(), cyclideCubeSideShader);
     }
 
@@ -83,7 +41,7 @@ void scenes::CyclidicCubeScene::RenderScene(const Renderer& renderer, glm::mat4 
         cyclideCubeSideShader.SetUnifromVec3f("lightPos", lightPos);
         cyclideCubeSideShader.SetUnifromVec3f("viewPos", viewPos);
         cyclideCubeSideShader.SetUnifromVec3f("lightColor", lightColor);
-        cyclideCubeSideShader.SetUnifromVec3f("objectColor", glm::vec3(1.0f, 0.3f, 1.0f));
+        cyclideCubeSideShader.SetUnifromVec3f("objectColor", staticSideColor);
         renderer.DrawTriangles(plain.GetVertexArray(), plain.GetIndexBuffer(), cyclideCubeSideShader);
     }
 
@@ -102,9 +60,11 @@ void scenes::CyclidicCubeScene::RenderScene(const Renderer& renderer, glm::mat4 
         cyclideCubeSideShader.SetUnifromVec3f("lightPos", lightPos);
         cyclideCubeSideShader.SetUnifromVec3f("viewPos", viewPos);
         cyclideCubeSideShader.SetUnifromVec3f("lightColor", lightColor);
-        cyclideCubeSideShader.SetUnifromVec3f("objectColor", glm::vec3(1.0f, 0.3f, 1.0f));
+        cyclideCubeSideShader.SetUnifromVec3f("objectColor", staticSideColor);
         renderer.DrawTriangles(plain.GetVertexArray(), plain.GetIndexBuffer(), cyclideCubeSideShader);
     }
+
+    renderer.ToggleWireFrame(wireframeToggle);
 
     //Side D
     {
@@ -121,7 +81,7 @@ void scenes::CyclidicCubeScene::RenderScene(const Renderer& renderer, glm::mat4 
         cyclideCubeSideShader.SetUnifromVec3f("lightPos", lightPos);
         cyclideCubeSideShader.SetUnifromVec3f("viewPos", viewPos);
         cyclideCubeSideShader.SetUnifromVec3f("lightColor", lightColor);
-        cyclideCubeSideShader.SetUnifromVec3f("objectColor", glm::vec3(1.0f, 0.3f, 1.0f));
+        cyclideCubeSideShader.SetUnifromVec3f("objectColor", dinamicSideColor);
         renderer.DrawTriangles(plain.GetVertexArray(), plain.GetIndexBuffer(), cyclideCubeSideShader);
     }
 
@@ -140,7 +100,7 @@ void scenes::CyclidicCubeScene::RenderScene(const Renderer& renderer, glm::mat4 
         cyclideCubeSideShader.SetUnifromVec3f("lightPos", lightPos);
         cyclideCubeSideShader.SetUnifromVec3f("viewPos", viewPos);
         cyclideCubeSideShader.SetUnifromVec3f("lightColor", lightColor);
-        cyclideCubeSideShader.SetUnifromVec3f("objectColor", glm::vec3(1.0f, 0.3f, 1.0f));
+        cyclideCubeSideShader.SetUnifromVec3f("objectColor", dinamicSideColor);
         renderer.DrawTriangles(plain.GetVertexArray(), plain.GetIndexBuffer(), cyclideCubeSideShader);
     }
 
@@ -159,21 +119,174 @@ void scenes::CyclidicCubeScene::RenderScene(const Renderer& renderer, glm::mat4 
         cyclideCubeSideShader.SetUnifromVec3f("lightPos", lightPos);
         cyclideCubeSideShader.SetUnifromVec3f("viewPos", viewPos);
         cyclideCubeSideShader.SetUnifromVec3f("lightColor", lightColor);
-        cyclideCubeSideShader.SetUnifromVec3f("objectColor", glm::vec3(1.0f, 0.3f, 1.0f));
+        cyclideCubeSideShader.SetUnifromVec3f("objectColor", dinamicSideColor);
         renderer.DrawTriangles(plain.GetVertexArray(), plain.GetIndexBuffer(), cyclideCubeSideShader);
     }
 
     renderer.ToggleWireFrame(false);
+}
+
+void scenes::CyclidicCubeScene::RenderPoints(const Renderer& renderer)
+{
+    glm::vec3 pointBallSize = glm::vec3(0.05f, 0.05f, 0.05f);
+    glm::vec3 staticPointBallColor = glm::vec3(0.88f, 0.58f, 0.47f);
+    glm::vec3 danamicPointBallColor = glm::vec3(0.92f, 0.96f, 0.97f);
+    glm::vec3 calculatedPointBallColor = glm::vec3(0.51f, 0.86f, 0.43f);
+
+    {
+        shadedColor.Bind();
+        glm::mat4 model = glm::scale(glm::translate(glm::mat4(1.0f), p0), pointBallSize);
+        glm::mat4 mvp = camera.GetProjectionView() * model;
+        shadedColor.SetUnifromMat4f("MVP", mvp);
+        shadedColor.SetUnifromVec3f("lightPos", lightPos);
+        shadedColor.SetUnifromVec3f("viewPos", viewPos);
+        shadedColor.SetUnifromVec3f("lightColor", lightColor);
+        shadedColor.SetUnifromVec3f("objectColor", staticPointBallColor);
+        renderer.DrawTriangles(sphere.GetVertexArray(), sphere.GetIndexBuffer(), shadedColor);
+    }
+    {
+        shadedColor.Bind();
+        glm::mat4 model = glm::scale(glm::translate(glm::mat4(1.0f), p1), pointBallSize);
+        glm::mat4 mvp = camera.GetProjectionView() * model;
+        shadedColor.SetUnifromMat4f("MVP", mvp);
+        shadedColor.SetUnifromVec3f("lightPos", lightPos);
+        shadedColor.SetUnifromVec3f("viewPos", viewPos);
+        shadedColor.SetUnifromVec3f("lightColor", lightColor);
+        shadedColor.SetUnifromVec3f("objectColor", staticPointBallColor);
+        renderer.DrawTriangles(sphere.GetVertexArray(), sphere.GetIndexBuffer(), shadedColor);
+    }
+    {
+        shadedColor.Bind();
+        glm::mat4 model = glm::scale(glm::translate(glm::mat4(1.0f), p2), pointBallSize);
+        glm::mat4 mvp = camera.GetProjectionView() * model;
+        shadedColor.SetUnifromMat4f("MVP", mvp);
+        shadedColor.SetUnifromVec3f("lightPos", lightPos);
+        shadedColor.SetUnifromVec3f("viewPos", viewPos);
+        shadedColor.SetUnifromVec3f("lightColor", lightColor);
+        shadedColor.SetUnifromVec3f("objectColor", staticPointBallColor);
+        renderer.DrawTriangles(sphere.GetVertexArray(), sphere.GetIndexBuffer(), shadedColor);
+    }
+    {
+        shadedColor.Bind();
+        glm::mat4 model = glm::scale(glm::translate(glm::mat4(1.0f), p3), pointBallSize);
+        glm::mat4 mvp = camera.GetProjectionView() * model;
+        shadedColor.SetUnifromMat4f("MVP", mvp);
+        shadedColor.SetUnifromVec3f("lightPos", lightPos);
+        shadedColor.SetUnifromVec3f("viewPos", viewPos);
+        shadedColor.SetUnifromVec3f("lightColor", lightColor);
+        shadedColor.SetUnifromVec3f("objectColor", danamicPointBallColor);
+        renderer.DrawTriangles(sphere.GetVertexArray(), sphere.GetIndexBuffer(), shadedColor);
+    }
+    {
+        shadedColor.Bind();
+        glm::mat4 model = glm::scale(glm::translate(glm::mat4(1.0f), p4), pointBallSize);
+        glm::mat4 mvp = camera.GetProjectionView() * model;
+        shadedColor.SetUnifromMat4f("MVP", mvp);
+        shadedColor.SetUnifromVec3f("lightPos", lightPos);
+        shadedColor.SetUnifromVec3f("viewPos", viewPos);
+        shadedColor.SetUnifromVec3f("lightColor", lightColor);
+        shadedColor.SetUnifromVec3f("objectColor", staticPointBallColor);
+        renderer.DrawTriangles(sphere.GetVertexArray(), sphere.GetIndexBuffer(), shadedColor);
+    }
+    {
+        shadedColor.Bind();
+        glm::mat4 model = glm::scale(glm::translate(glm::mat4(1.0f), p5), pointBallSize);
+        glm::mat4 mvp = camera.GetProjectionView() * model;
+        shadedColor.SetUnifromMat4f("MVP", mvp);
+        shadedColor.SetUnifromVec3f("lightPos", lightPos);
+        shadedColor.SetUnifromVec3f("viewPos", viewPos);
+        shadedColor.SetUnifromVec3f("lightColor", lightColor);
+        shadedColor.SetUnifromVec3f("objectColor", danamicPointBallColor);
+        renderer.DrawTriangles(sphere.GetVertexArray(), sphere.GetIndexBuffer(), shadedColor);
+    }
+    {
+        shadedColor.Bind();
+        glm::mat4 model = glm::scale(glm::translate(glm::mat4(1.0f), p6), pointBallSize);
+        glm::mat4 mvp = camera.GetProjectionView() * model;
+        shadedColor.SetUnifromMat4f("MVP", mvp);
+        shadedColor.SetUnifromVec3f("lightPos", lightPos);
+        shadedColor.SetUnifromVec3f("viewPos", viewPos);
+        shadedColor.SetUnifromVec3f("lightColor", lightColor);
+        shadedColor.SetUnifromVec3f("objectColor", danamicPointBallColor);
+        renderer.DrawTriangles(sphere.GetVertexArray(), sphere.GetIndexBuffer(), shadedColor);
+    }
+    {
+        shadedColor.Bind();
+        glm::mat4 model = glm::scale(glm::translate(glm::mat4(1.0f), p7), pointBallSize);
+        glm::mat4 mvp = camera.GetProjectionView() * model;
+        shadedColor.SetUnifromMat4f("MVP", mvp);
+        shadedColor.SetUnifromVec3f("lightPos", lightPos);
+        shadedColor.SetUnifromVec3f("viewPos", viewPos);
+        shadedColor.SetUnifromVec3f("lightColor", lightColor);
+        shadedColor.SetUnifromVec3f("objectColor", calculatedPointBallColor);
+        renderer.DrawTriangles(sphere.GetVertexArray(), sphere.GetIndexBuffer(), shadedColor);
+    }
+}
+
+void scenes::CyclidicCubeScene::CalculatePoints()
+{
+    p6 = mathCalc::CalculatePointInCircle(sideA, (p6Slider * 0.99 + 0.25) * glm::pi<float>());
+    p3 = mathCalc::CalculatePointInCircle(sideB, (p3Slider * 0.99 - 0.249) * glm::pi<float>());
+    p5 = mathCalc::CalculatePointInCircle(sideC, (p5Slider * 0.99 - 0.75) * glm::pi<float>());
+
+    mathCalc::CircleData sideF = mathCalc::GetCircleDataFrom3Points(p1, p3, p5);
+    mathCalc::CircleData sideE = mathCalc::GetCircleDataFrom3Points(p4, p5, p6);
+    mathCalc::CircleData sideD = mathCalc::GetCircleDataFrom3Points(p2, p3, p6);
+
+    mathCalc::Plane planeF = mathCalc::GetPlaneFromCircleData(sideF);
+    mathCalc::Plane planeE = mathCalc::GetPlaneFromCircleData(sideE);
+    mathCalc::Plane planeD = mathCalc::GetPlaneFromCircleData(sideD);
+
+    p7 = mathCalc::IntersectionPointFrom3Planes(
+        planeF,
+        planeE,
+        planeD
+    );
+}
+
+scenes::CyclidicCubeScene::CyclidicCubeScene() :
+    plain(30),
+    sphere(1, 10, 10),
+    cyclideCubeSideShader("res/shaders/CyclideCubeSide.shader"),
+    shadedColor("res/shaders/ShadedColor.shader"),
+    p3Slider(0.505f),
+    p5Slider(0.505f),
+    p6Slider(0.505f),
+    sideA(mathCalc::GetCircleDataFrom3Points(p0, p2, p4)),
+    sideB(mathCalc::GetCircleDataFrom3Points(p0, p1, p2)),
+    sideC(mathCalc::GetCircleDataFrom3Points(p0, p4, p1))
+{
+}
+
+void scenes::CyclidicCubeScene::RenderScene(const Renderer& renderer, glm::mat4 newView)
+{
+	glm::mat4& view = camera.GetView();
+	view = newView;
+
+    glm::vec3 viewPos = newView[3];
+    glm::vec3 lightPos = glm::vec3(0.0f, 10.0f, 16.0f);
+    glm::vec3 lightColor = glm::vec3(1.0f, 1.0f, 1.0f);
+
+    RenderGizmo(renderer, newView);
+
+    CalculatePoints();
+
+    if (cubeToggle)
+    {
+        RenderCube(renderer);
+    }
+
+    if (pointToggle)
+    {
+        RenderPoints(renderer);
+    }
 
     ImGui::Begin("Cyclidic Cube Controls");
-    //ImGui::SliderFloat3("v1", &v1.x, -1.0f, 1.0f);
-    //ImGui::SliderFloat3("v2", &v2.x, -1.0f, 1.0f);
     ImGui::SliderFloat("Point p3", &p3Slider, 0.0f, 1.0f);
     ImGui::SliderFloat("Point p5", &p5Slider, 0.0f, 1.0f);
     ImGui::SliderFloat("Point p6", &p6Slider, 0.0f, 1.0f);
-    //ImGui::SliderFloat("Point p4", &p4Slider, 0.0f, 1.0f);
-    //ImGui::SliderInt("Size", &size, -10, 10);
-    //ImGui::SliderFloat("Angle", &angle, -5.0f, 5.0f);
     ImGui::Checkbox("Wireframe", &wireframeToggle);
+    ImGui::Checkbox("Render points", &pointToggle);
+    ImGui::Checkbox("Render cube", &cubeToggle);
     ImGui::End();
 }
--- GraphicsEngine/src/scenes/CyclidicCubeScene.h
+++ GraphicsEngine/src/scenes/CyclidicCubeScene.h
@@ -3,6 +3,7 @@
 #include <geometries/PlaneGeometry.h>
 #include <geometries/SphereGeometry.h>
 #include <geometries/TubeGeometry.h>
+#include <vendor/MathCalculations.h>
 
 namespace scenes
 {
@@ -11,9 +12,13 @@ namespace scenes
 		float p3Slider;
 		float p5Slider;
 		float p6Slider;
-		bool wireframeToggle;
-		glm::vec3 v1;
-		glm::vec3 v2;
+		bool wireframeToggle = false;
+		bool pointToggle = false;
+		bool cubeToggle = true;
+
+		glm::vec3 viewPos;
+		glm::vec3 lightPos = glm::vec3(0.0f, 10.0f, 16.0f);
+		glm::vec3 lightColor = glm::vec3(1.0f, 1.0f, 1.0f);
 
 		glm::vec3 p0 = glm::vec3(0.0f, 0.0f, 0.0f);
 		glm::vec3 p1 = glm::vec3(0.0f, 0.0f, 1.0f);
@@ -25,12 +30,20 @@ namespace scenes
 		glm::vec3 p6;
 		glm::vec3 p7;
 
+		mathCalc::CircleData sideA;
+		mathCalc::CircleData sideB;
+		mathCalc::CircleData sideC;
 
 		geometry::PlaneGeometry plain;
 		//geometry::TubeGeometry tube;
-		//geometry::SphereGeometry sphere;
+		geometry::SphereGeometry sphere;
 
 		Shader cyclideCubeSideShader;
+		Shader shadedColor;
+
+		void RenderCube(const Renderer& renderer);
+		void RenderPoints(const Renderer& renderer);
+		void CalculatePoints();
 
 	public:
 		CyclidicCubeScene();
